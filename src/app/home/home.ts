0603240f7d6c4447339724dfbc28ef609fb7206b
Ilhom Mirtojiyev <mirtojiyevilhom@gmail.com>
2025-12-21 20:34:35 +0500

    upgrede 6

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BgComponent } from '../bg-component/bg-component';
+import { Hr } from '../hr/hr';
 
 @Component({
   selector: 'app-home',
-  imports: [BgComponent],
+  imports: [BgComponent,Hr],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
---
+++ src/app/model/hr.ts
@@ -0,0 +1,8 @@
+export interface HrInterface {
+    id:number,
+    title:string,
+    text:string,
+    link:string,
+    img:string,
+    active:boolean
+}
