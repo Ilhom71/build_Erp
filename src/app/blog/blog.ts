5e12c230e404b7847061750f3761c3ded5f32c5b
Ilhom Mirtojiyev <mirtojiyevilhom@gmail.com>
2025-12-27 10:42:19 +0500

    upgrade 11

--- src/app/blog/blog.ts
+++ src/app/blog/blog.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LoginCard } from "../login-card/login-card";
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [LoginCard],
   templateUrl: './blog.html',
   styleUrl: './blog.css',
 })
